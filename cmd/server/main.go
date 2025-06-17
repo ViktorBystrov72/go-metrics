04410c3054cea0fa40a8224d6f69845eb6f8acaa
Виктор Глушак <v.glushak@vk.team>
2025-06-17 23:55:41 +0500

    Iter 1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,94 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+type MetricType string
+
+const (
+	Gauge   MetricType = "gauge"
+	Counter MetricType = "counter"
+)
+
+type MemStorage struct {
+	mu       sync.Mutex
+	gauges   map[string]float64
+	counters map[string]int64
+}
+
+func NewMemStorage() *MemStorage {
+	return &MemStorage{
+		gauges:   make(map[string]float64),
+		counters: make(map[string]int64),
+	}
+}
+
+func (s *MemStorage) UpdateGauge(name string, value float64) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.gauges[name] = value
+}
+
+func (s *MemStorage) UpdateCounter(name string, value int64) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.counters[name] += value
+}
+
+func main() {
+	storage := NewMemStorage()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/update/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		// /update/<type>/<name>/<value>
+		parts := strings.Split(r.URL.Path, "/")
+		if len(parts) != 5 {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		metricType := parts[2]
+		name := parts[3]
+		value := parts[4]
+
+		if name == "" {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/plain")
+
+		switch metricType {
+		case string(Gauge):
+			v, err := strconv.ParseFloat(value, 64)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			storage.UpdateGauge(name, v)
+		case string(Counter):
+			v, err := strconv.ParseInt(value, 10, 64)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				return
+			}
+			storage.UpdateCounter(name, v)
+		default:
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		_, _ = fmt.Fprint(w, "OK")
+	})
+
+	http.ListenAndServe(":8080", mux)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ViktorGlushak/go-metrics
+
+go 1.23.0
